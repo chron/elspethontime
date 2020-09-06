0370f26be1afb1bb28a82f46e402d11f6ef25e4a
Paul Prestidge <paul.prestidge@flickelectric.co.nz>
2020-09-07 08:52:07 +1200

    Refactor image info for the states into its own data file

---
+++ src/_data/states.js
@@ -0,0 +1,27 @@
+module.exports =  {
+  yes: {
+    image: '/trophy.svg',
+    alt: 'Trophy icon',
+    title: 'She made it!',
+  },
+  no: {
+    image: '/skull.svg',
+    alt: 'Skull icon',
+    title: 'She didn\'t make it.',
+  },
+  wfh: {
+    image: '/house.svg',
+    alt: 'House icon',
+    title: 'She worked from home this day.',
+  },
+  sick: {
+    image: '/heart-broken.svg',
+    alt: 'Broken heart icon',
+    title: 'She wasn\'t feeling well and had the day off.',
+  },
+  unknown: {
+    image: '/question-square.svg',
+    alt: 'Question mark inside a square',
+    title: 'Who knows???',
+  },
+}
